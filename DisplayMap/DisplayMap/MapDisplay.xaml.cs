98b55b74e0314c2cbc114e14feb776e9a8c44f8d
sangitamalviya02 <sangitamalviya02@gmail.com>
2020-04-08 20:11:48 +0530

    My First Commit

---
+++ DisplayMap/DisplayMap/MapDisplay.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Esri.ArcGISRuntime.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DisplayMap
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MapDisplay : ContentPage
+	{
+		public MapDisplay ()
+		{
+			InitializeComponent ();
+            Initialize();
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            _ = Navigation.PopAsync();
+        }
+         async void Initialize()
+        {
+            Map uxMap = null;
+            ArcGISTiledLayer tiledLayer = new ArcGISTiledLayer(new Uri("https://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer"));
+            await tiledLayer.LoadAsync();
+            Basemap basemap = new Basemap(tiledLayer);
+            uxMap = new Map(basemap);
+            MyMapView.Map = uxMap;
+        }
+    }
+}
